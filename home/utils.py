ef8b225615f411a6f4d63674e5d19975417cfe6f
agentcdp <33668010+agentcdp@users.noreply.github.com>
2023-04-22 17:26:14 +0530

    Update utils.py

--- home/utils.py
+++ home/utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-
+# Linear Regression model for waist prediction
 class WaistSizePredictor:
     def __init__(self, data: list):
         self.model = LinearRegression()
