dd042c0d8a907d45db210ace7905ecabde2da44b
agentcdp <33668010+agentcdp@users.noreply.github.com>
2023-04-22 17:23:00 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,7 +3,12 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
+    # For index page
     path('', views.Index.as_view(), name='index'),
+    
+    # Get waist size
     path('get_waist/', views.get_waist, name="get_waist"),
+    
+    # Update waist size
     path('update_waist/', views.update_waist, name="update_waist"),
 ]
