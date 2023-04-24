0a2f2e9c876652f1d9e6edadcfd6bdf85eb78b4b
agentcdp <33668010+agentcdp@users.noreply.github.com>
2023-04-24 17:35:18 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -23,16 +23,24 @@ def validate_float_inputs(value):
 # Get waist detail
 def get_waist(request):
     if request.method == "GET":
+        # get post data
         height, age, weight = request.GET.get('height', ''), request.GET.get('age', ''), request.GET.get('weight', '')
+        # Validate waist
         validate_float_inputs(height), validate_float_inputs(age), validate_float_inputs(weight)
         try:
+            # init predictor
             predictor = WaistSizePredictor(data=get_measurement_data())
+            # setting up user input data
             predictor.set_prediction_data(height, age, weight)
+            # prediction details
             predicted_waist = predictor.predict()
+            # return response as json
             return JsonResponse({"waist": round(predicted_waist[0], 2)}) 
         except Exception as e:
+            # raise error if any issue
             raise Http404     
     else:
+        # raise error if request is not GET
         raise Http404
         
 # Update waist
@@ -46,13 +54,16 @@ def update_waist(request):
         try:
             # Create new object
             m, created = Measurement.objects.get_or_create(height=height, weight=weight, age=age, waist=waist)
+            # checking object is a new object or not, if new, this will be stored as user created item for filtering the data in the future
             if created:
                 m.user_created = True
                 m.save()
+            # returning json response of model
             return JsonResponse(m.get_json())
         except Exception as e:
             raise Http404
     else:
+        # raise error if request is not POST
         raise Http404
         
 # Index page
@@ -60,6 +71,7 @@ class Index(TemplateView):
     template_name = "home/index.html"
 
     def get_context_data(self, **kwargs):
+        # object for first load
         page_object = Measurement.objects.filter().first()
         context = super().get_context_data(**kwargs)
         context['page_object'] = page_object
