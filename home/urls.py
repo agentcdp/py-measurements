7b70c030499e2fb596c7a5f8558d9d48d525e20b
agentcdp <33668010+agentcdp@users.noreply.github.com>
2023-04-24 17:30:40 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     # For index page
     path('', views.Index.as_view(), name='index'),
     
-    # Get waist size
+    # Get waist size for on input field change
     path('get_waist/', views.get_waist, name="get_waist"),
     
-    # Update waist size
+    # Update waist size if user submit new update request
     path('update_waist/', views.update_waist, name="update_waist"),
 ]
